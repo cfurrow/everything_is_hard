216f7acf054aafe27fd0e1153eb98c03f813bdb5
Carl Furrow <carl.furrow@gmail.com>
2017-10-13 12:26:10 -0400

    Download

--- src/index.js
+++ src/index.js
@@ -52,4 +52,6 @@ var binary_gif = encoder.stream().getData(); //notice this is different from the
 var data_url = 'data:image/gif;base64,'+encode64(binary_gif);
 encoder.finish();
 img.src = data_url;
-//encoder.download("download.gif");
+
+
+encoder.download("download.gif");
